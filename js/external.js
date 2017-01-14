4b8ee37c72759f1fd6da9fd25784eb2c1f4d52ab
Boris <boris.krivoruchko@gmail.com>
2017-01-14 15:40:12 +0200

    initial commit

---
+++ js/external.js
@@ -0,0 +1,102 @@
+/*alert('ошибка');
+[1,2].forEach(alert)*/
+// коментарий  cmd +"/"
+/* комментарий на
+2 и более строк alt+cmd+"/"*/
+/*var admin, name, greeting
+name="vasiliy";
+admin=name;
+greeting="Hello dear "+admin;
+alert(greeting);*/
+
+
+/*var ourPlanetName, visitorName;
+ourPlanetName="Earth";
+visitorName="Vasiliy";
+alert(visitorName+" on "+ourPlanetName);
+*/
+/*var n=4; 
+
+n +=5;// n=n+5 =(8)
+
+alert (n %=2); //n= n/2 = 4*/
+
+/*var yourName=prompt('What is your name','Alex?');
+confirm("Are you sure you are "+yourName);
+alert("OK "+yourName);
+*/
+/*var year=prompt('what year is it now?','')
+
+if (year==2016) {
+	alert ("nope");
+} 
+	else {
+		alert("nope");
+	};
+*/
+
+/*var year=prompt('what year is it now?','')
+
+if (year>2016) {
+	alert ("too late");
+} 
+	else if (year<2016){
+		alert("too early");
+	}else
+	alert('right');*/
+
+/*var yourAge= prompt('Age','')
+var message= (yourAge>18)? 'Welcome':
+	(yourAge<18)? 'Restricted':
+	'not sure';
+
+alert(message);
+*/
+
+
+/*
+var login=prompt('your login','');
+
+if (login=='admin') {
+	var password=prompt ('enter password','');
+	if (password=='black_master'){
+		alert('Welcome'+password);
+	}else if (password==null) {
+		alert('canceled');
+	}else {
+		alert('Invalid password');
+
+	}
+} else if (login==null) {
+	alert('login canceled');
+} else{
+	alert('login Invalid');
+}
+*/
+
+var a=prompt("a",'');
+var b=prompt('b');
+result=(a + b < 4) ?
+'Мало':
+'Много';
+alert (result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
